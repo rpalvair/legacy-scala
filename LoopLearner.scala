2caa7aed49dc09ce9e472af501bbee9ec4b3643e
widdy <r.palvair@gmail.com>
2014-12-22 17:53:40 +0100

    For loops

--- LoopLearner.scala
+++ LoopLearner.scala
@@ -15,6 +15,31 @@ class LoopLearner {
       }
     }
   }
+
+  def executeFor(args: Array[String]): Unit = {
+    println("execute for")
+    if (args != null) {
+      for (i <- 0 to args.length - 1) {
+        println("arg {" + i + "} = " + args(i));
+      }
+    }
+  }
+
+  def executeFor2(args: Array[String]): Unit = {
+    println("execute for2")
+    if (args != null) {
+      for (arg <- args) {
+        println("arg = " + arg);
+      }
+    }
+  }
+
+  def executeForEach(args: Array[String]): Unit = {
+    println("execute forEach")
+    if (args != null) {
+      args.foreach((arg: String) => println("arg : " + arg))
+    }
+  }
 }
 
 object LoopLearner extends App {
@@ -22,8 +47,14 @@ object LoopLearner extends App {
   val loopLearner = new LoopLearner
   if (args.length > 0) {
     loopLearner.executeWhile(args)
+    loopLearner.executeFor(args)
+    loopLearner.executeFor2(args)
+    loopLearner.executeForEach(args)
   } else {
     loopLearner.executeWhile(null)
+    loopLearner.executeFor(null)
+    loopLearner.executeFor2(null)
+    loopLearner.executeForEach(null)
   }
 }
 
