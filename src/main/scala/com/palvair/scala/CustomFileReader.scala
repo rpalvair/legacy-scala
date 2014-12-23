d80ff27b84166b7a7258356f794acb86dde12df1
widdy <r.palvair@gmail.com>
2014-12-23 16:04:17 +0100

    Added file reader + enhancement of HelloWorld.scala

---
+++ src/main/scala/com/palvair/scala/CustomFileReader.scala
@@ -0,0 +1,15 @@
+package main.scala.com.palvair.scala
+
+import scala.io.Source._
+
+/**
+ * Created by rpalvair on 23/12/2014.
+ */
+class CustomFileReader {
+  def readFile(fileName: String): Unit = {
+    val lines = fromFile(fileName).getLines
+    for (line <- lines) {
+      println("line = [" + line + "]")
+    }
+  }
+}
--- src/main/scala/com/palvair/scala/HelloWorld.scala
+++ src/main/scala/com/palvair/scala/HelloWorld.scala
@@ -1,5 +1,7 @@
 package com.palvair.scala
 
+import main.scala.com.palvair.scala.CustomFileReader
+
 /**
  * Created by rpalvair on 19/12/2014.
  */
@@ -8,9 +10,25 @@ class HelloWorld {
   def answer(): Unit = {
     println("You called me?");
   }
+
+  def readFile(): Unit = {
+    val reader = new CustomFileReader
+    reader.readFile("../README.md")
+  }
+
+  def callListLearner(): Unit = {
+    val listLearner = new ListLearner
+    listLearner.arrayInitialization()
+    listLearner.initList()
+    listLearner.listConcatenation()
+    listLearner.listPrepend("widdy says : ")
+    listLearner.parameterizeArray()
+  }
 }
 
 object HelloWorld extends App {
   val helloWorld = new HelloWorld
   helloWorld.answer()
+  helloWorld.readFile()
+  helloWorld.callListLearner()
 }
