43d0b2b0b3764dba9bf6286e1ae6186ea3c322c4
widdy <r.palvair@gmail.com>
2015-01-08 14:02:41 +0100

    Added Rational class

---
+++ src/main/scala/com/palvair/scala/Rational.scala
@@ -0,0 +1,21 @@
+package com.palvair.scala
+
+/**
+ * Created by rpalvair on 08/01/2015.
+ */
+class Rational(n: Int, d: Int) {
+  require(d != 0)
+  val numer: Int = n
+  val denom: Int = d
+
+  def this(n: Int) = this(n, 1);
+
+  override def toString = numer + "/" + denom
+
+  def add(input: Rational): Rational = {
+    new Rational(
+      this.numer * input.denom + input.numer * this.denom,
+      this.denom * input.denom
+    )
+  }
+}
